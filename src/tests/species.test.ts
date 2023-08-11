35d69ab4305949b2811debdb3913b35412e5f3d9
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-11 20:30:57 +0100

    test to return string passes

--- src/tests/species.test.ts
+++ src/tests/species.test.ts
@@ -1,5 +1,11 @@
+import { getEvolutionURL } from "../services/evolution_url";
+
 describe('getEvolution', () => {
-  it('should return the evolution URL for a given species', () => {
-    
+  it('should return the evolution URL for a given species', async () => {
+    const species = 'metapod';
+
+    const evolutionURL = await getEvolutionURL(species);
+
+    expect(evolutionURL).toBe('https://pokeapi.co/api/v2/evolution-chain/4/')
   })
 })
