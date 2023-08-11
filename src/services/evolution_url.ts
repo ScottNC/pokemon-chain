7b0ea1ff70c2246e946051df5838c6bf632e31ff
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-11 21:06:36 +0100

    jest mocks axios return to get evolution url

--- src/services/evolution_url.ts
+++ src/services/evolution_url.ts
@@ -1,3 +1,12 @@
+import axios, { AxiosResponse } from "axios";
+import { POKEMON_URL } from "../config";
+
 export async function getEvolutionURL (species: string) {
-  return 'https://pokeapi.co/api/v2/evolution-chain/4/';
+  try {
+    const response: AxiosResponse = await axios.get(POKEMON_URL + '/pokemon-species/' + species);
+    const data: any = response.data;
+    return data.evolution_chain.url
+  } catch (error) {
+    throw new Error('Failed to fetch evolution URL');
+  }
 }
--- src/tests/species.test.ts
+++ src/tests/species.test.ts
@@ -1,11 +1,26 @@
+import { POKEMON_URL } from "../config";
 import { getEvolutionURL } from "../services/evolution_url";
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('getEvolution', () => {
   it('should return the evolution URL for a given species', async () => {
-    const species = 'metapod';
+
+    const pokemonSpeciesData = {
+      data: {
+        evolution_chain : {
+          url: POKEMON_URL + '/evolution-chain/12341234/'
+        }
+      }
+    };
+
+    (axios.get as jest.Mock).mockResolvedValue(pokemonSpeciesData);
+
+    const species = 'noodles';
 
     const evolutionURL = await getEvolutionURL(species);
 
-    expect(evolutionURL).toBe('https://pokeapi.co/api/v2/evolution-chain/4/')
+    expect(evolutionURL).toBe(POKEMON_URL + '/evolution-chain/12341234/')
   })
 })
